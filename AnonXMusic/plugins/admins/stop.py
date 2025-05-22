2eb9d4f2d820bafa40402ac874aac7cbcc3021af
ahmet1346zz1 <sonsuzmango01@gmail.com>
2025-05-22 19:06:42 +0300

    wtrtwer

--- AnonXMusic/plugins/admins/stop.py
+++ AnonXMusic/plugins/admins/stop.py
@@ -9,9 +9,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(
-    filters.command(["son", "cson"]) & filters.group & ~BANNED_USERS
-)
+@app.on_message(filters.command(["son", "cson"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
