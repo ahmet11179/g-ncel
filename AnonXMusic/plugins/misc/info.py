b09e08132b991340c4758b2fee472c4e8924659c
ahmet1346zz1 <sonsuzmango01@gmail.com>
2025-05-22 20:55:35 +0300

    asdfasd

--- AnonXMusic/plugins/misc/info.py
+++ AnonXMusic/plugins/misc/info.py
@@ -20,13 +20,13 @@ async def userinfo(_, message):
             id = user_info.id
             dc_id = user.dc_id
             first_name = user_info.first_name 
-            last_name = user_info.last_name if user_info.last_name else "No last name"
-            username = user_info.username if user_info.username else "No Username"
+            last_name = user_info.last_name if user_info.last_name else "Soyadı yok"
+            username = user_info.username if user_info.username else "Kullanıcı adı yok"
             mention = user.mention
-            bio = user_info.bio if user_info.bio else "No bio set"
+            bio = user_info.bio if user_info.bio else "Biyografi ayarlanmamış"
             
             if user.photo:
-                # User has a profile photo
+                # Kullanıcının profil fotoğrafı varsa
                 photo = await app.download_media(user.photo.big_file_id)
                 welcome_photo = await get_userinfo_img(
                     bg_path=bg_path,
@@ -35,7 +35,7 @@ async def userinfo(_, message):
                     profile_path=photo,
                 )
             else:
-                # User doesn't have a profile photo, use random_photo directly
+                # Kullanıcının profil fotoğrafı yoksa, rastgele bir fotoğraf kullan
                 welcome_photo = random.choice(random_photo)
                 
             await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
@@ -51,13 +51,13 @@ async def userinfo(_, message):
             id = user_info.id
             dc_id = user.dc_id
             first_name = user_info.first_name 
-            last_name = user_info.last_name if user_info.last_name else "No last name"
-            username = user_info.username if user_info.username else "No Username"
+            last_name = user_info.last_name if user_info.last_name else "Soyadı yok"
+            username = user_info.username if user_info.username else "Kullanıcı adı yok"
             mention = user.mention
-            bio = user_info.bio if user_info.bio else "No bio set"
+            bio = user_info.bio if user_info.bio else "Biyografi ayarlanmamış"
             
             if user.photo:
-                # User has a profile photo
+                # Kullanıcının profil fotoğrafı varsa
                 photo = await app.download_media(user.photo.big_file_id)
                 welcome_photo = await get_userinfo_img(
                     bg_path=bg_path,
@@ -66,7 +66,7 @@ async def userinfo(_, message):
                     profile_path=photo,
                 )
             else:
-                # User doesn't have a profile photo, use random_photo directly
+                # Kullanıcının profil fotoğrafı yoksa, rastgele bir fotoğraf kullan
                 welcome_photo = random.choice(random_photo)
                 
             await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
@@ -75,34 +75,4 @@ async def userinfo(_, message):
             await message.reply_text(str(e))
 
     elif message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        try:
-            user_info = await app.get_chat(user_id)
-            user = await app.get_users(user_id)
-            status = await userstatus(user.id)
-            id = user_info.id
-            dc_id = user.dc_id
-            first_name = user_info.first_name 
-            last_name = user_info.last_name if user_info.last_name else "No last name"
-            username = user_info.username if user_info.username else "No Username"
-            mention = user.mention
-            bio = user_info.bio if user_info.bio else "No bio set"
-            
-            if user.photo:
-                # User has a profile photo
-                photo = await app.download_media(user.photo.big_file_id)
-                welcome_photo = await get_userinfo_img(
-                    bg_path=bg_path,
-                    font_path=font_path,
-                    user_id=user.id,
-                    profile_path=photo,
-                )
-            else:
-                # User doesn't have a profile photo, use random_photo directly
-                welcome_photo = random.choice(random_photo)
-                
-            await app.send_photo(chat_id, photo=welcome_photo, caption=INFO_TEXT.format(
-                id, first_name, last_name, username, mention, status, dc_id, bio), reply_to_message_id=message.id)
-        except Exception as e:
-            await message.reply_text(str(e))
-                
+        user_id =_
