946f9d2a937421ee23a97a452966b9a30329315a
ahmet1346zz1 <sonsuzmango01@gmail.com>
2025-05-22 18:31:45 +0300

    fadfa

--- AnonXMusic/plugins/oyun/dicegame.py
+++ AnonXMusic/plugins/oyun/dicegame.py
@@ -4,31 +4,37 @@ from AnonXMusic import app as app
 
 @app.on_message(filters.command("zar"))
 async def dice(bot, message):
+    await message.delete()  # Kullanıcının yazdığı komutu sil
     x = await bot.send_dice(message.chat.id)  # Zar atılır
     m = x.dice.value  # Zarın sonucu alınır
   
 @app.on_message(filters.command("dart"))
 async def dart(bot, message):
+    await message.delete()  # Kullanıcının yazdığı komutu sil
     x = await bot.send_dice(message.chat.id, "🎯")  # Dart oyunu
     m = x.dice.value  # Sonuç alınır
 
 @app.on_message(filters.command("basket"))
 async def basket(bot, message):
+    await message.delete()  # Kullanıcının yazdığı komutu sil
     x = await bot.send_dice(message.chat.id, "🏀")  # Basketbol oyunu
     m = x.dice.value  # Sonuç alınır
 
-@app.on_message(filters.command("sulot"))
+@app.on_message(filters.command("slot"))
 async def jackpot(bot, message):
+    await message.delete()  # Kullanıcının yazdığı komutu sil
     x = await bot.send_dice(message.chat.id, "🎰")  # Jackpot (slot makinesi)
     m = x.dice.value  # Sonuç alınır
 
 @app.on_message(filters.command("Bowling"))
 async def ball(bot, message):
+    await message.delete()  # Kullanıcının yazdığı komutu sil
     x = await bot.send_dice(message.chat.id, "🎳")  # Bowling oyunu
     m = x.dice.value  # Sonuç alınır
 
 @app.on_message(filters.command("football"))
 async def football(bot, message):
+    await message.delete()  # Kullanıcının yazdığı komutu sil
     x = await bot.send_dice(message.chat.id, "⚽")  # Futbol oyunu
     m = x.dice.value  # Sonuç alınır
 
@@ -39,7 +45,7 @@ Emoji ile Oyun Oynayın:
 /basket - Basketbol 🏀
 /Bowling - Bowling 🎳
 /football - Futbol ⚽
-/sulot - Slot makinesi döndür 🎰
+/slot - Slot makinesi döndür 🎰
 """
 
 __mod_name__ = "Dɪᴄᴇ"
